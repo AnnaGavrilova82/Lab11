9b5f4610927416dc99eb452737a8469322afb5b4
IliaGavrilov <IliaGavrilov@ILIAGAVRILOV-HP>
2021-10-31 16:49:26 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     
+            Root root= new Root();
+            Console.WriteLine ("Введите значение К  ");
+            root.K = Console.ReadLine(); 
+            Console.Write("Введите значение B  ");
+            root.B = Console.ReadLine();
+            root.Result();
+            Console.ReadKey();
+        }
+
+        struct Root
+        {
+            public double k;
+            public double b;
+            double result;
+
+            public string K
+            {
+                set
+                {
+                    if (Convert.ToDouble(value).GetType() == typeof(double))
+                    {
+                        k = Convert.ToDouble(value);
+                        Console.WriteLine(k);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верный тип данных");
+
+                    }
+                }
+                get
+                {
+                    return k;
+                }
+            }
+            public string B
+            {
+                set
+                {
+                    if (Convert.ToDouble(value).GetType() == typeof(double))
+                    {
+                        b = Convert.ToDouble(value);
+                        Console.WriteLine(b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верный тип данных");
+
+                    }
+                }
+                get
+                {
+                    return b;
+                }
+            }
+
+            public Root()
+            {
+                result = (-b) / k;
+            }
+            public void Result()
+            {
+                Console.WriteLine("Значение X: {0}", result);
+            }
+        } 
+    }
+}
